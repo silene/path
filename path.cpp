747bd1f8063a2663d12138431eb8ceea5b9f5c67
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2026-08-21 18:23:43 +0200

    Make it possible to zoom.

--- path.cpp
+++ path.cpp
@@ -2334,28 +2334,29 @@ struct camera {
 struct simple: camera {
   vec pos;
   mat rot;
-  simple(vec const &p, mat const &r)
-    : pos(p), rot(r) {}
-  simple(vec const &p, vec const &t, vec const &y)
-    : pos(p), rot(Matrix::rotation_zy(t - p, y)) {}
-  simple(vec const &p, vec const &t)
-    : simple(p, t, vec { 0., 1., 0. }) {}
+  double zoom;
+  simple(vec const &p, mat const &r, double z)
+    : pos(p), rot(r), zoom(1. / (3. * z)) {}
+  simple(vec const &p, vec const &t, double z, vec const &y)
+    : simple(p, Matrix::rotation_zy(t - p, y), z) {}
+  simple(vec const &p, vec const &t, double z)
+    : simple(p, t, z, vec { 0., 1., 0. }) {}
   std::pair<vec, vec> get(double fx, double fy) const;
 };
 
 std::pair<vec, vec> simple::get(double fx, double fy) const {
-  vec d { fx, fy, 1. };
+  vec d { fx * zoom, fy * zoom, 1. };
   return { pos, normalize(rot * d) };
 }
 
 struct simple_lens: simple {
   double lens, focal;
-  simple_lens(vec const &p, mat const &r, double l, double f)
-    : simple(p, r), lens(l), focal(f) {}
-  simple_lens(vec const &p, vec const &t, double l, vec const &y)
-    : simple(p, t, y), lens(l), focal(norm(t - p)) {}
-  simple_lens(vec const &p, vec const &t, double l)
-    : simple_lens(p, t, l, vec { 0., 1., 0. }) {}
+  simple_lens(vec const &p, mat const &r, double z, double l, double f)
+    : simple(p, r, z), lens(l), focal(f) {}
+  simple_lens(vec const &p, vec const &t, double z, double l, vec const &y)
+    : simple(p, t, z, y), lens(l), focal(norm(t - p)) {}
+  simple_lens(vec const &p, vec const &t, double z, double l)
+    : simple_lens(p, t, z, l, vec { 0., 1., 0. }) {}
   std::pair<vec, vec> get(double fx, double fy) const;
 };
 
@@ -2365,7 +2366,7 @@ std::pair<vec, vec> simple_lens::get(double fx, double fy) const {
   // that would have been targeted if there was no lens.
   point2 l = Sampler::disk_uniform();
   vec s = lens * vec { l[0], l[1], 0. };
-  vec d { fx, fy, 1. };
+  vec d { fx * zoom, fy * zoom, 1. };
   d = focal * d - s;
   return { pos + rot * s, normalize(rot * d) };
 }
