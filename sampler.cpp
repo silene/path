2baa0c4051ebcaad61b7ac6a7119c18c5c281888
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2026-08-26 08:05:05 +0200

    Move away from rejection-based sampling.

--- sampler.cpp
+++ sampler.cpp
@@ -89,27 +89,38 @@ int discrete_uniform::sample() {
   return v;
 }
 
+/*
+triangle O - (1,Y) - (1,Y+dY) with Y = y/x
+gets mapped to
+triangle O - (cos t, sin t) - (cos (t + dt), sin (t + dt)) for some t
+
+equality of relative areas -> dY/8 = dt/2pi
+t = pi/4 Y = pi/4 y/x
+*/
+
 point2 disk_uniform() {
   std::uniform_real_distribution dis(-1., 1.);
-  point2 res;
-  for (;;) {
-    res[0] = dis(*rng);
-    res[1] = dis(*rng);
-    double l = res[0] * res[0] + res[1] * res[1];
-    if (l > 1) continue;
-    return res;
+  double x = dis(*rng), y = dis(*rng);
+  if (x == 0 && y == 0) return { 0., 0. };
+  double r, t;
+  if (std::abs(y) > std::abs(x)) {
+    r = y;
+    t = 2. - x / y;
+  } else {
+    r = x;
+    t = y / x;
   }
+  t *= M_PI * 0.25;
+  return { r * cos(t), r * sin(t) };
 }
 
 biased<vec> sphere_uniform::sample() const {
-  std::uniform_real_distribution dis(-1., 1.);
-  for (;;) {
-    vec w;
-    for (int i = 0; i < 3; ++i) { w[i] = dis(*rng); }
-    double l = norm(w);
-    if (l > 1 || l < 1e-6) continue;
-    return { (1 / l) * w, 0.25 * M_1_PI };
-  }
+  std::uniform_real_distribution dis(0., 1.);
+  double z = 2. * dis(*rng) - 1.;
+  double r = sqrt(1. - z * z);
+  double t = 2. * M_PI * dis(*rng);
+  vec v { r * cos(t), r * sin(t), z };
+  return { v, 0.25 * M_1_PI };
 }
 
 double sphere_uniform::pdf(vec const &) const {
@@ -117,15 +128,14 @@ double sphere_uniform::pdf(vec const &) const {
 }
 
 biased<vec> hemisphere_uniform::sample() const {
-  std::uniform_real_distribution dis(-1., 1.);
-  for (;;) {
-    vec w;
-    for (int i = 0; i < 3; ++i) { w[i] = dis(*rng); }
-    double l = norm(w);
-    if (l > 1 || l < 1e-6) continue;
-    if ((u | w) < 0) l = -l;
-    return { (1 / l) * w, 0.5 * M_1_PI };
-  }
+  vec uu = std::abs(u[0]) < 0.5 ? vec { 1., 0., 0. } : vec { 0., 1., 0. };
+  Matrix::mat rot = Matrix::rotation_zy(u, uu);
+  std::uniform_real_distribution dis(0., 1.);
+  double z = dis(*rng);
+  double r = sqrt(1. - z * z);
+  double t = 2. * M_PI * dis(*rng);
+  vec v { r * cos(t), r * sin(t), z };
+  return { rot * v, 0.5 * M_1_PI };
 }
 
 double hemisphere_uniform::pdf(vec const &v) const {
@@ -133,22 +143,20 @@ double hemisphere_uniform::pdf(vec const &v) const {
   return 0.5 * M_1_PI;
 }
 
+/*
+z = sqrt(1 - (x² + y²)) with x,y ~ disk
+
+P(z >= Z) = P_disk(r <= sqrt(1 - Z²)) = 1 - Z²
+A(z >= Z) = int_(cos t >= Z) 2pi sin t dt = 2pi (1 - Z)
+p = dP/dA = dP/dZ / dA/dZ = Z/pi
+*/
+
 biased<vec> hemisphere_linear::sample() const {
-  std::uniform_real_distribution dis(-1., 1.);
-  for (;;) {
-    vec w;
-    for (int i = 0; i < 3; ++i) { w[i] = dis(*rng); }
-    double l = norm(w);
-    if (l > 1 || l < 1e-6) continue;
-    w = (1 / l) * w;
-    double s = (u | w);
-    if (s < 0) { w = -w; s = -s; }
-    // w - s * u is orthogonal to u, of norm sqrt(1 - s²)
-    double ss = sqrt(s);
-    double f = 1 / sqrt(1 + s);
-    // return a unit vector r such that r|u = ss = sqrt(w|u)
-    return { f * w + (ss - f * s) * u, ss * M_1_PI };
-  }
+  vec uu = std::abs(u[0]) < 0.5 ? vec { 1., 0., 0. } : vec { 0., 1., 0. };
+  Matrix::mat rot = Matrix::rotation_zy(u, uu);
+  point2 xy = disk_uniform();
+  double z = sqrt(1. - xy[0]*xy[0] - xy[1]*xy[1]);
+  return { rot * vec { xy[0], xy[1], z }, z * M_1_PI };
 }
 
 double hemisphere_linear::pdf(vec const &v) const {
@@ -156,6 +164,7 @@ double hemisphere_linear::pdf(vec const &v) const {
   return (u | v) * M_1_PI;
 }
 
+#if 0
 biased<vec> hemisphere_power::sample() const {
   std::uniform_real_distribution dis(-1., 1.);
   for (;;) {
@@ -178,20 +187,26 @@ double hemisphere_power::pdf(vec const &v) const {
   if ((u | v) <= 0) return 0.;
   return (p + 1) * exp(log(u | v) * p) * 0.5 * M_1_PI;
 }
+#endif
 
+/*
+z ~ U(cmax, 1)
+r = sqrt(1 - z²)
+x, y = r * (cos t, sin t) with t ~ U(0, 2pi)
+
+P(z >= Z) = (1 - Z) / (1 - cmax)
+A(z >= Z) = int_(cos t >= Z) 2pi sin t dt = 2pi (1 - Z)
+p = dP/dA = dP/dZ / dA/dZ = 1 / (2pi (1 - cmax))
+*/
 biased<vec> cone_uniform::sample() const {
-  std::uniform_real_distribution dis(-1., 1.);
-  std::uniform_real_distribution dis2(0., 1.);
-  for (;;) {
-    vec w;
-    for (int i = 0; i < 3; ++i) { w[i] = dis(*rng); }
-    vec v = cross(u, w);
-    double d = norm(v);
-    if (d < 1e-6) continue;
-    double c = 1 + dis2(*rng) * (cmax - 1);
-    v *= sqrt(1 - c * c) / d;
-    return { v + c * u, 1 / (2 * M_PI * (1 - cmax)) };
-  }
+  vec uu = std::abs(u[0]) < 0.5 ? vec { 1., 0., 0. } : vec { 0., 1., 0. };
+  Matrix::mat rot = Matrix::rotation_zy(u, uu);
+  std::uniform_real_distribution dis(0., 1.);
+  double z = 1. + dis(*rng) * (cmax - 1.);
+  double r = sqrt(1. - z * z);
+  double t = 2. * M_PI * dis(*rng);
+  vec v { r * cos(t), r * sin(t), z };
+  return { rot * v, 1. / (2. * M_PI * (1. - cmax)) };
 }
 
 double cone_uniform::pdf(vec const &v) const {
